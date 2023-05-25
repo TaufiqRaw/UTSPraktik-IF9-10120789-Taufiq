90b648847745deb0920ebae0c2bedc3d965cd5df
taufiqraw <waskitaufiq@gmail.com>
2023-05-25 10:01:11 +0700

    Initial commit

---
+++ app/src/main/java/com/taufiqraw/utspraktik_if9_10120789_taufiq/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.taufiqraw.utspraktik_if9_10120789_taufiq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+* Tanggal Pengerjaan : 25/05/2023
+* Nama    : Taufiq Ridho Adi Waskita
+* NIM     : 10120789
+* Kelas   : IF-9
+*/
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
